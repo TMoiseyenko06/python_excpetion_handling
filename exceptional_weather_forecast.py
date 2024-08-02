83f5226369b1a07da972b08ea3dcbb9a3c8f9c0e
TMoiseyenko06 <tmoiseyenko06@gmail.com>
2024-08-02 15:53:10 -0400

    Added missing string

--- exceptional_weather_forecast.py
+++ exceptional_weather_forecast.py
@@ -4,7 +4,7 @@ def main():
         temperature_F = int(input("Please enter a temperature in Farenheit:"))
         temperature_C = (temperature_F - 32) * 5/9
     except:
-        print("Please Enter a number!")
+        print("Please Enter a valid number!")
     else:
         print(f'{temperature_F} degrees farenheit is {temperature_C} in celsius!')
     finally:
